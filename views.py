ad5d3c9876e89a8927944995ba3e03d234b86bfb
Cynthia Gail McDonald <mcdonaldgail@live.com>
2020-06-12 18:14:23 -0700

    old code override undone.

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UnicodeTextForm
-import unicodedata as ud
-from .mappings import bidi,category
+from .uni import get_normalization_form, examen_unicode
+
 
 def search(request):
-    """View for home page and search."""
+    """Home and search."""
     if request.method == 'POST':
         form = UnicodeTextForm(request.POST)
         if form.is_valid():
@@ -13,100 +13,18 @@ def search(request):
             normalization_form = get_normalization_form(text)
             text = examen_unicode(text)
             return render(request, 'search.html', {'form': form,
-                                                            'text': text,
-                                                            'version': ud.unidata_version,
-                                                            'normalization_form': normalization_form,})
+                                                    'text': text,
+                                                    'normalization_form': normalization_form,
+                                                    'title' : 'Unicode Search',
+                                                    'tagline' : 'Examine a Unicode String'})
     form = UnicodeTextForm()
-    return render(request, 'search.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
 
-
-def examen_unicode(text):
-    """Returns a list of dictionaries for each unicode character."""
-    char_list = []
-    for char in text:
-        if ord(char) == 0xa:
-            # If character is a LINE FEED.
-            char_dict = {
-            'char': char,
-            'name': 'LINE FEED',
-            'category': category[ud.category(char)],
-            'digit': '',
-            'bidi': '',
-            'ord': ord(char),
-            'code_point': 'U+' + str(hex(ord(char))),
-            }
-            char_list.append(char_dict)
-
-        elif ord(char) == 0xd:
-            # If character is a Carriage Return.
-            char_dict = {
-            'char': char,
-            'name': 'CARRIAGE RETURN',
-            'category': category[ud.category(char)],
-            'digit': '',
-            'bidi': '',
-            'ord': ord(char),
-            'code_point': 'U+' + str(hex(ord(char))),
-            }
-            char_list.append(char_dict)
-        elif ord(char) == 0x20:
-            # If character is SPACE.
-            char_dict = {
-            'char': char,
-            'name': 'SPACE',
-            'category': category[ud.category(char)],
-            'digit': '',
-            'bidi': '',
-            'ord': ord(char),
-            'code_point': 'U+' + str(hex(ord(char))),
-            }
-            char_list.append(char_dict)
-        else:
-            # Default.
-            try:
-                char_dict = {
-                'char': char,
-                'name': ud.name(char),
-                'category': category[ud.category(char)],
-                'digit': ud.digit(char, ''),
-                'bidi': bidi[ud.bidirectional(char)],
-                'ord': ord(char),
-                'code_point': 'U+' + str(hex(ord(char))),
-                }
-                char_list.append(char_dict)
-            except:
-                char_dict = {
-                'char': char,
-                'name': 'UNKNOWN',
-                'category': '',
-                'digit': '',
-                'bidi': '',
-                'ord': ord(char),
-                'code_point': 'U+' + str(hex(ord(char))),
-                }
-                char_list.append(char_dict)
-
-    return char_list
-
-def get_normalization_form(string):
-    """Return Dictionary of Normalization Forms"""
-
-    normalization_form = {
-        'NFC': False,
-        'NFKC': False,
-        'NFD': False,
-        'NFKD': False,
-    }
-
-    for form in normalization_form:
-        if ud.is_normalized(form, string) == True:
-            normalization_form[form] = True
-    return normalization_form
 
 def tofu(request):
-    'View for tofu page.'
+    """Tofu page."""
     return render(request, 'tofu.html', {'title' : 'Tofu',
-                                         'tagline' : 'Not just for eating'})
+                                         'tagline' : 'Not Just For Eating'})
 
 def about(request):
     'View function for about page of site.'
