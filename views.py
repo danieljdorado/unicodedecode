80185e7c992410ca311863a2873ad81b9d3f122f
Daniel Dorado <danieljdorado@yahoo.com>
2020-07-20 11:37:14 -0700

    Rename Search module to 'decode'.

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 """Views."""
 
 from django.shortcuts import render
-from Search.forms import UnicodeTextForm
-import Search.unicode_util as u
+from decode.forms import UnicodeTextForm
+import decode.unicode_util as u
 
 
 def about(request):
     """About Page."""
-    return render(request, 'about.html', {'title' : 'About',
+    return render(request, 'decode/about.html', {'title' : 'About',
                                          'tagline' : 'Get To Know Us'})
 
 
@@ -16,39 +16,39 @@ def character(request, slug):
 
     char = chr(int(slug, 16))
     char_desc = u.get_character_page_description(char)
-    return render(request, 'character.html', char_desc)
+    return render(request, 'decode/character.html', char_desc)
 
 def privacy(request):
         """Terms and Conditions Page."""
-        return render(request, 'privacy.html', {'title' : 'Privacy Policy',
+        return render(request, 'decode/privacy.html', {'title' : 'Privacy Policy',
                                               'tagline' : "We're Not Tracking"})
 
 
-def search(request):
-    """Home and search."""
+def decode(request):
+    """Home and decode."""
     if request.method == 'POST':
         form = UnicodeTextForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['text']
             normalization_form = u.get_normalization_form(text)
             text = u.examen_unicode(text)
-            return render(request, 'search.html', {'form': form,
+            return render(request, 'decode/decode.html', {'form': form,
                                                    'text': text,
                                                    'normalization_form': normalization_form,
                                                    'title' : 'Unicode Decode',
                                                    'tagline' : 'Decode a Unicode String'})
 
     form = UnicodeTextForm()
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'decode/home.html', {'form': form})
 
 
 def terms(request):
         """Terms and Conditions Page."""
-        return render(request, 'terms.html', {'title' : 'Terms and Conditions',
+        return render(request, 'decode/terms.html', {'title' : 'Terms and Conditions',
                                               'tagline' : 'User Agreements'})
 
 
 def tofu(request):
     """Tofu page."""
-    return render(request, 'tofu.html', {'title' : 'Tofu',
+    return render(request, 'decode/tofu.html', {'title' : 'Tofu',
                                          'tagline' : 'Not Just For Eating'})
